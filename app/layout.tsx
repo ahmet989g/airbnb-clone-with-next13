84e080af89840e392eb75fd426669dc94334738c
ahmet989g <ahmet.erden98@gmail.com>
2023-09-09 02:32:18 +0300

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Modal actionLabel="Submit" title="Login Modal" isOpen/>
+          <Navbar />
+        </ClientOnly>
         {children}
         </body>
     </html>
